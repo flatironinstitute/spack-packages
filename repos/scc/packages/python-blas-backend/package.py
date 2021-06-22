f105d56e58454a80a7098a78726342b3140c47e7
Dylan Simon <dylan@dylex.net>
2021-06-22 10:50:35 -0400

    python-blas-backend: remove hard-coded versions

--- repos/scc/packages/python-blas-backend/package.py
+++ repos/scc/packages/python-blas-backend/package.py
@@ -27,9 +27,9 @@ class PythonBlasBackend(BundlePackage):
     """FIXME: Put a proper description of your package here."""
 
     # FIXME: Add proper versions here.
-    version('3.8.10')
+    version('1')
 
     # FIXME: Add dependencies if required.
-    depends_on('python@3.8.10')
-    depends_on('py-numpy@1.20.3')
-    depends_on('py-scipy@1.6.3')
+    depends_on('python')
+    depends_on('py-numpy')
+    depends_on('py-scipy')
