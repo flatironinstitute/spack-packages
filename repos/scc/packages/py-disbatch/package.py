15173451ae9b32aca3ff564ba698244bdb78254b
Robert Blackwell <rblackwell@flatironinstitute.org>
2021-06-23 16:48:46 -0400

    Add disBatch 2.0-rc1

--- repos/scc/packages/py-disbatch/package.py
+++ repos/scc/packages/py-disbatch/package.py
@@ -30,6 +30,7 @@ class PyDisbatch(PythonPackage):
     url      = "https://github.com/flatironinstitute/disBatch/archive/refs/tags/1.4.tar.gz"
 
     version('1.4', sha256='ce3a1079f895ddc0029cd21db3a5f4bf07602392a7587bdf99e4e04834e91516')
+    version('2.0-rc1', sha256='d95bcfdb23d5652cd35714e5aaf71d66dc1f39d4486c814567ad80daec4e8484')
     depends_on('py-kvsstcp', type=('run'))
 
     @run_after('install')
