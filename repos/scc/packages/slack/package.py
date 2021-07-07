c97716829c6283d3b5586d56b9d674903c92568e
Alex Chavkin <achavkin@scclin013.flatironinstitute.org>
2021-07-07 14:39:34 -0400

    Add slack
    Maybe there's a better way to do this?

---
+++ repos/scc/packages/slack/package.py
@@ -0,0 +1,15 @@
+from spack import *
+from shutil import move
+from os import getcwd
+
+class Slack(Package):
+    """FI Slack no-sandbox"""
+
+    url      = "/mnt/home/spack/cache/slack/slack-4.17.0-0.1.fc21.x86_64.tar.xz"
+
+    maintainers = ['alexdotc']
+
+    version('4.17.0-0.1.fc21', sha256='6657bad3c0532c606c3fb32b03800167c7c533684c848431101b88524656254f')
+
+    def install(self, spec, prefix):
+        move(getcwd(), prefix)
