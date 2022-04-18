40b03e7074305f969a7a84349ddb5064f64d40ab
Dylan Simon <dylan@dylex.net>
2022-04-18 17:37:05 -0400

    readline: use ncursesw; python: revert #27375

--- repos/builtin/packages/python/package.py
+++ repos/builtin/packages/python/package.py
@@ -516,13 +516,6 @@ class Python(Package):
                 string=True,
             )
 
-        if '^ncurses' in self.spec:
-            with open('Modules/Setup', 'a') as sf:
-                # make sure the curses module uses ncursesw (#27369)
-                sf.write("*shared*\n"
-                         "_curses _cursesmodule.c -lncursesw\n"
-                         "_curses_panel _curses_panel.c -lpanel -lncursesw\n")
-
     def setup_build_environment(self, env):
         spec = self.spec
 
@@ -666,11 +659,6 @@ class Python(Package):
             # libffi reside in prefix.lib but the configure script of Python
             # finds them using pkg-config.
             cppflags = " ".join("-I" + spec[dep.name].prefix.include for dep in link_deps)
-
-            if '^ncurses' in spec:
-                # make sure we get ncursesw (first)
-                cppflags = '-I{0}/ncursesw '.format(spec['ncurses'].prefix.include) \
-                        + cppflags
 
             # Currently, the only way to get SpecBuildInterface wrappers of the
             # dependencies (which we need to get their 'libs') is to get them
