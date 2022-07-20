3608ac1acdee6688dbbc9aff4958e982dfc2a563
Dylan Simon <dylan@dylex.net>
2022-07-20 17:42:56 -0400

    python: re-apply part of #27375
    still need to force ncurses include before system!

--- repos/builtin/packages/python/package.py
+++ repos/builtin/packages/python/package.py
@@ -660,6 +660,11 @@ class Python(Package):
             # finds them using pkg-config.
             cppflags = " ".join("-I" + spec[dep.name].prefix.include for dep in link_deps)
 
+            if '^ncurses' in spec:
+                # make sure we get ncursesw (first)
+                cppflags = '-I{0}/ncursesw '.format(spec['ncurses'].prefix.include) \
+						+ cppflags
+
             # Currently, the only way to get SpecBuildInterface wrappers of the
             # dependencies (which we need to get their 'libs') is to get them
             # using spec.__getitem__.
