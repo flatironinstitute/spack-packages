db46306b0ee52254a148ebe13f9c0e4b53583292
Dylan Simon <dylan@dylex.net>
2023-12-10 22:03:06 -0500

    py-jupyterlab: add 3.6.6

--- repos/builtin/packages/py-jupyterlab/package.py
+++ repos/builtin/packages/py-jupyterlab/package.py
@@ -17,6 +17,7 @@ class PyJupyterlab(PythonPackage):
     license("BSD-3-Clause")
 
     version("4.0.1", sha256="4dc3901f7bbfd4704c994b7a893a49955256abf57dba9831f4825e3f3165b8bb")
+    version("3.6.6", sha256="0a47d7adb28bd5659d727783f4113537e54f8c66e0d6322d1d8f9edb081dc926")
     version("3.4.8", sha256="1fafb8b657005d91603f3c3adfd6d9e8eaf33fdc601537fef09283332efe67cb")
     version("3.4.2", sha256="38abd3a4f83a8f97e3f15bebbcc0825903c15519809eedfaa41340d260be2160")
     version("3.2.9", sha256="65ddc34e5da1a764606e38c4f70cf9d4ac1c05182813cf0ab2dfea312c701124")
@@ -75,6 +76,9 @@ class PyJupyterlab(PythonPackage):
         # dependency on py-jinja2@2.1 seems to be a migration issue from the switch
         # to setup.cfg in 3.0.15, leave it a 2.10
         depends_on("py-jinja2@2.10:", type=("build", "run"))
+
+        depends_on("py-jupyter-ydoc@0.2.4:0.2")
+        depends_on("py-jupyter-server-ydoc@0.8")
 
     with when("@:2"):
         depends_on("python@:3.9", when="@:2.1", type=("build", "run"))
