346499311f39a196009f92e23df01f916b37d829
Lehman Garrison <lgarrison@flatironinstitute.org>
2025-03-15 22:26:03 -0400

    py-classylss: use fork for python@3.12:

--- repos/builtin/packages/py-classylss/package.py
+++ repos/builtin/packages/py-classylss/package.py
@@ -11,16 +11,25 @@ class PyClassylss(PythonPackage):
     homepage = "https://github.com/nickhand/classylss"
     pypi = "classylss/classylss-0.2.9.tar.gz"
 
+    version("0.2.9.1", git="https://github.com/sbird/classylss.git", commit="6006fabb5abeab2611ab3300a5c927f447e9c9f0")
     version("0.2.9", sha256="1a8521d2bf9da3d2572245e801e243fcf76f7518b59cbe525a31aa80a884dd86")
 
+    depends_on("python@:3.11", type=("build", "run"), when="@=0.2.9")
+
+    depends_on("py-setuptools@42:", type="build", when="@0.2.9.1:")
     depends_on("py-setuptools", type="build")
     depends_on("py-cython", type="build")
 
     depends_on("py-numpy", type=("build", "run"))
     depends_on("py-six", type=("build", "run"))
 
-    patch('setup.py.patch', sha256='2ccf53b90f76cfd4643877bd9952c474dad1ac8a0a2d9daf2bee29c36972dec0')
+    patch('setup.py.patch', sha256='2ccf53b90f76cfd4643877bd9952c474dad1ac8a0a2d9daf2bee29c36972dec0', when="@=0.2.9")
 
+    @when("@0.2.9.1")
+    def patch(self):
+        filter_file(r"'language': 'c'", "'language': 'c++'", "setup.py")
+
+    @when("@=0.2.9")
     def patch(self):
         # Shouldn't rely on the Cythonized output being portable
         remove('classylss/binding.c')
