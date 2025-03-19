bc68e497049c1309ad97896d76fa15c8602d074d
Lehman Garrison <lgarrison@flatironinstitute.org>
2025-03-19 17:03:16 -0400

    healpix-cxx: add v3.83, update packaging

--- repos/builtin/packages/healpix-cxx/package.py
+++ repos/builtin/packages/healpix-cxx/package.py
@@ -5,27 +5,59 @@
 from spack.package import *
 
 
-class HealpixCxx(AutotoolsPackage):
-    """Healpix-CXX is a C/C++ library for calculating
+class HealpixCxx(Package):
+    """Healpix is a library for calculating
     Hierarchical Equal Area isoLatitude Pixelation of a sphere."""
 
     homepage = "https://healpix.sourceforge.io"
-    url = "https://ayera.dl.sourceforge.net/project/healpix/Healpix_3.50/healpix_cxx-3.50.0.tar.gz"
+    url = "https://downloads.sourceforge.net/project/healpix/Healpix_3.82/Healpix_3.82_2022Jul28.tar.gz"
 
-    license("GPL-2.0-or-later")
+    license("GPL-2.0-or-later", checked_by="lgarrison")
 
-    version("3.50.0", sha256="6538ee160423e8a0c0f92cf2b2001e1a2afd9567d026a86ff6e2287c1580cb4c")
+    version(
+        "3.83",
+        url="https://downloads.sourceforge.net/project/healpix/Healpix_3.83/Healpix_3.83_2024Nov13.tar.gz",
+        sha256="8876c18efc596fd706b2a004ac15f2fb60b795f2db6fbabea9d8ccf549531dda",
+    )
+    version("3.82", sha256="47629f057a2daf06fca3305db1c6950edb9e61bbe2d7ed4d98ff05809da2a127")
 
     depends_on("cfitsio")
-    depends_on("libsharp", type="build")
+
+    phases = ["configure", "install"]
 
     def patch(self):
-        spec = self.spec
-        configure_fix = FileFilter("configure")
-        # Link libsharp static libs
-        configure_fix.filter(
-            r"^SHARP_LIBS=.*$",
-            'SHARP_LIBS="-L{0} -lsharp -lc_utils -lfftpack -lm"'.format(
-                spec["libsharp"].prefix.lib
-            ),
+        prefix = self.prefix
+        config = FileFilter("hpxconfig_functions.sh")
+        config.filter(r"^\s*SHARPPREFIX=.*", f"SHARPPREFIX={prefix}")
+        config.filter(r"^\s*CXXPREFIX=.*", f"CXXPREFIX={prefix}")
+        config.filter(r'SHARP_LIBS="[^"]*"', f'SHARP_LIBS="-L{prefix.lib} -lsharp"')
+        config.filter(r'SHARP_CFLAGS="[^"]*"', f'SHARP_CFLAGS="-I{prefix.include}"')
+        config.filter(r"^\s*F90_BINDIR=.*", f"F90_BINDIR={prefix.bin}")
+        config.filter(r"^\s*F90_INCDIR=.*", f"F90_INCDIR={prefix.include}")
+        config.filter(r"^\s*F90_LIBDIR=.*", f"F90_LIBDIR={prefix.lib}")
+        config.filter(r'^\s*ExtendCFLAGS "-I.*\/include"', f'ExtendCFLAGS "-I{prefix.include}"')
+
+    def configure(self, spec, prefix):
+        configure = Executable("./configure")
+        configure(*self.configure_args(), extra_env=self.configure_env(spec))
+
+    def configure_args(self):
+        return ["-L", "--auto=c,cxx,f90"]
+
+    def configure_env(self, spec):
+        return dict(
+            SHELL="sh",
+            C_SHARED="1",
+            FITSDIR=spec["cfitsio"].prefix.lib,
+            FITSINC=spec["cfitsio"].prefix.include,
+            SHARP_COPT="-O3 -ffast-math",
+            F_SHARED="1",
         )
+
+    def install(self, spec, prefix):
+        make = Executable("make")
+        make("-j", str(make_jobs), "all", *self.make_args())
+
+    def make_args(self):
+        prefix = self.prefix
+        return [f"C_LIBDIR={prefix.lib}", f"C_INCDIR={prefix.include}"]
